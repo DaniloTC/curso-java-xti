5f76bede821bde13f15db2b787383258c06c1969
Danilo Castro <danilotrindade87@gmail.com>
2018-12-24 20:09:04 -0200

    Comentario editado

--- 003 Tipos Primitivos/Variavel.java
+++ 003 Tipos Primitivos/Variavel.java
@@ -36,8 +36,8 @@ public class Variavel {
 			| int       -2147483648             2147483647                 4 bytes |
 			| long      -9223372036854775808L   9223372036854775807L       8 bytes |
 			========================================================================
-			| double	1.4 elevado a -45       3.4 elevado a 38           4 bytes |
-			| float		4.9 elevado a -324      1.7 elevado a 308          8 bytes |
+			| double    1.4 elevado a -45       3.4 elevado a 38           4 bytes |
+			| float     4.9 elevado a -324      1.7 elevado a 308          8 bytes |
 			========================================================================
 		*/
 		
