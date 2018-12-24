a601ecdf2c12f5d449b3addcd01263b9ec48fa13
Danilo Castro <danilotrindade87@gmail.com>
2018-12-24 20:22:56 -0200

    Comentarios editado

--- 003 Tipos Primitivos/Variavel.java
+++ 003 Tipos Primitivos/Variavel.java
@@ -41,16 +41,16 @@ public class Variavel {
 			========================================================================
 		*/
 		
-		byte varByte     		  = 127; // cem
-		short varShort    		  = 32767; // 32 mil
-		int varInt        		  = 2_000_000_000; // 2 bilhões
-		long varLong      		  = 9_000_000_000_000_000_000L; // 9 quintilhões
+		byte varByte              = 127; // cem
+		short varShort            = 32767; // 32 mil
+		int varInt                = 2_000_000_000; // 2 bilhões
+		long varLong              = 9_000_000_000_000_000_000L; // 9 quintilhões
 		long varLongComValorMenor = 9_000_000L; // Long definido com tamanho menor que int
 		
 		/* PADRÃO IEEE 754
 		   Esse padrão mostra como os números com ponto flutuante (double e float) devem
 		   ser tratados pelo computador. Além disso, a letra no final do valor serve para
-		   indicar a qual tipo um valor pertence (F para float), (D para double).
+		   indicar a qual tipo um valor pertence (F ou f para float), (D ou d para double).
 		*/
 		
 		double varDouble  = 1.7976931348623157E+308d; // Padrão IEEE 754
@@ -68,8 +68,8 @@ public class Variavel {
 		*/
 		
 		//varInt = varShort; //Cast Implícito
-		//System.out.println(varShort); // tipo maior com valor menor armazenado
-		//System.out.println(varInt);       // colocado dentro de um tipo menor
+		//System.out.println(varShort); // tipo de dado MENOR
+		//System.out.println(varInt);   // colocado dentro de outro tipo de dado MAIOR
 		
 		/*	CONVERTENDO VARIAVEIS (Parte 2)
 		
@@ -80,27 +80,26 @@ public class Variavel {
 			Esse tipo de conversão consiste em colocar o valor de uma variável do tipo maior dentro de
 			outra variável do tipo menor.
 			Nesse tipo de conversão um não cabe dentro do outro e por isso esse tipo de Cast é conhecido
-			como Explícito, pois há a necessidade de deixar explícito que o programador realmente quer
-			realizar essa conversão, mesmo que isso signifique perda de valores.
+			como Explícito, pois há a necessidade de deixar explícito para o Java que o programador 
+			realmente quer realizar essa conversão, mesmo que isso signifique perda de valores.
 		*/
 		
-		/* UM TIPO NAO CABE DENTRO DO OUTRO, POR ISSO HA PERDA DE VALOR */
+		/* EXEMPLO 1: Um tipo NAO CABE dentro do outro, por isso ha perda de valor* /
 		
-		//varInt = (int) varLong; // Necessário forçar a conversão com (<tipo>)
+		//varInt = (int) varLong; // Necessário forçar a conversão (ha perda de valor)
 		//System.out.println(varLong); // tipo maior
 		//System.out.println(varInt);  // colocado dentro de um tipo menor
 		
-		/* UM TIPO NAO CABE DENTRO DO OUTRO, MAS O VALOR SIM, POR ISSO, NAO HA PERDA DE VALOR */
+		/* EXEMPLO 2: Um tipo NAO CABE dentro do outro, mas o VALOR SIM, por isso, nao ha perda de valor */
 		
-		//varInt = (int) varLongComValorMenor; // Agora um cabe dentro do outro (há perda de valor)
+		//varInt = (int) varLongComValorMenor;
 		//System.out.println(varInt);
 		//System.out.println(varLongComValorMenor);
 		
 		/*	SISTEMA DE NUMERACAO BINARIO
 			
 			Para representar um valor no formato binário é preciso iniciar o valor da variável
-			com os caracteres'0b', dessa forma o computador saberá que esse valor é do tipo
-			binário.
+			com os caracteres '0b'.
 			
 			=======================================================================
 			| TIPO      VALOR BINARIO                           TAMANHO           |
