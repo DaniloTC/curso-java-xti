e64b365ed82d03af681bf7d3a56d10974a5ff2b2
Danilo Castro <danilotrindade87@gmail.com>
2019-01-04 19:01:11 -0200

    Entendendo os operadores de atribuição

---
+++ 012 Operadores de Atribuicao/Operador.java
@@ -0,0 +1,33 @@
+public class Operador {
+    public static void main(String[] args){
+        /*
+            OPERADORES DE ATRIBUICAO
+
+            +=      Adição
+            -=      Subtração
+            *=      Multiplicação
+            /=      Divisão
+            %=      Módulo
+        */
+
+        // Base variable
+        int x = 6;
+
+        //x = x + 3;    //o código da linha seguinte é equivalente a este
+        //x += 3;       //o código da linha anterior é equivalente a este
+
+        //x = x - 3;    //o código da linha seguinte é equivalente a este
+        //x -= 3;       //o código da linha anterior é equivalente a este
+
+        //x = x * 3;    //o código da linha seguinte é equivalente a este
+        //x *= 3;       //o código da linha anterior é equivalente a este
+
+        //x = x / 3;    //o código da linha seguinte é equivalente a este
+        //x /= 3;       //o código da linha anterior é equivalente a este
+
+        //x = x % 3;    //o código da linha seguinte é equivalente a este
+        //x %= 3;       //o código da linha anterior é equivalente a este
+
+        System.out.println(x);
+    }
+}
